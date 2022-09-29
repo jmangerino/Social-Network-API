fc55bb8050970d535fdcb2200fcc082f868c0db2
Josh Man <joshuamangerino@gmail.com>
2022-09-28 19:09:32 -0600

    finishing up server.js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -3,11 +3,8 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
-// Add `/users` to created routes 
 router.use('/user', userRoutes);
-
-// Add `/thoughts` to created routes 
+ 
 router.use('/thought', thoughtRoutes);
 
-// Export Module Router
 module.exports = router;
